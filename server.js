73a85785ef5670bf612f031601d5f7859d6ddd7a
Carolina RC <caro.rmz315@gmail.com>
2018-06-04 22:07:10 -0500

    selected the item in the browser, loop through each story and save to object

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ var app = express();
 // morgan logger for loggin requests CHECK IF LATER ISSUE 
 // app.use(logger("dev"));
 
+// body parser chunks your data (it's a middleware) in a secure way, because express by itself cannot read all daa
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true })) //set extended to true to prevent stripping objects
 //serve static client css and js files
@@ -40,19 +41,41 @@ app.set('view engine', 'handlebars');
 // get route for scraping the medium site 
 app.get("/scrape", function(req, res) {
     // first we grab the body of the html with the request
-    axios.get("http://www.medium.com/").then(function(response) {
+    axios.get("http://www.techcrunch.com").then(function(response) {
         // then we load that into cheerio and save it to $ for a shorthand selector
         var $ = cheerio.load(response.data);
     
     // grab every h3 within an a class
-    $("h3").each(function(i, element) {
-        // save an empty result object
-        var result = {};
+    // $("h3").each(function(i, element) {
+    //     // save an empty result object
 
         // add the text and href of every link , and save them 
         // as properties of the result object 
+        let stories = $(".post-block__title__link"); 
+        console.log(stories);
 
-    })
+        // load text of stories - check cheerio
+        stories.each(function(story, i) {
+            console.log($(this).text());
+            console.log($(this).attr("href"))
+
+            // mongoose to save this on db
+            
+            var result = {
+                    title: $(this).text(),
+                    url: $(this).attr("href")
+                };
+
+            // create my collections in db
+            db.Collection.create(result);        
+
+        })
+            // close off function and inform user once done (keep it restful)
+
+            // retrive items from db 
+
+
+   // })
 })
 
 })
